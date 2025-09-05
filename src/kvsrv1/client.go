60f5bee0a50d733f272bd223619da2ad04302f1c
akane <1297228257@qq.com>
2025-09-05 21:31:28 +0800

    feat(kvsrv1): 实现 KV 的 Put/Get 简单 RPC 调用

--- src/kvsrv1/client.go
+++ src/kvsrv1/client.go
@@ -2,8 +2,8 @@ package kvsrv
 
 import (
 	"6.5840/kvsrv1/rpc"
-	"6.5840/kvtest1"
-	"6.5840/tester1"
+	kvtest "6.5840/kvtest1"
+	tester "6.5840/tester1"
 )
 
 
@@ -30,6 +30,13 @@ func MakeClerk(clnt *tester.Clnt, server string) kvtest.IKVClerk {
 // arguments. Additionally, reply must be passed as a pointer.
 func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 	// You will have to modify this function.
+	args := rpc.GetArgs{Key: key}
+	reply := rpc.GetReply{}
+	ok := ck.clnt.Call(ck.server, "KVServer.Get", &args, &reply)
+	if ok {
+		// return value, version, err
+		return reply.Value, reply.Version, reply.Err
+	}
 	return "", 0, rpc.ErrNoKey
 }
 
@@ -52,5 +59,11 @@ func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 // arguments. Additionally, reply must be passed as a pointer.
 func (ck *Clerk) Put(key, value string, version rpc.Tversion) rpc.Err {
 	// You will have to modify this function.
+	args := rpc.PutArgs{Key: key, Value: value, Version: version}
+	reply := rpc.PutReply{}
+	ok := ck.clnt.Call(ck.server, "KVServer.Put", &args, &reply)
+	if ok {
+		return reply.Err
+	}
 	return rpc.ErrNoKey
 }
--- src/kvsrv1/server.go
+++ src/kvsrv1/server.go
@@ -6,7 +6,7 @@ import (
 
 	"6.5840/kvsrv1/rpc"
 	"6.5840/labrpc"
-	"6.5840/tester1"
+	tester "6.5840/tester1"
 )
 
 const Debug = false
@@ -18,15 +18,19 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
+type KVEntry struct {
+	Value string
+	Version rpc.Tversion
+}
 
 type KVServer struct {
-	mu sync.Mutex
-
+	Mu sync.Mutex
+	Data map[string]KVEntry;
 	// Your definitions here.
 }
 
 func MakeKVServer() *KVServer {
-	kv := &KVServer{}
+	kv := &KVServer{Data: make(map[string]KVEntry)}
 	// Your code here.
 	return kv
 }
@@ -35,6 +39,17 @@ func MakeKVServer() *KVServer {
 // exists. Otherwise, Get returns ErrNoKey.
 func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 	// Your code here.
+	key := args.Key
+	kv.Mu.Lock()
+	defer kv.Mu.Unlock()
+	entry, ok := kv.Data[key]
+	if ok {
+		reply.Value = entry.Value
+		reply.Version = entry.Version
+		reply.Err = rpc.OK
+	} else {
+		reply.Err = rpc.ErrNoKey
+	}
 }
 
 // Update the value for a key if args.Version matches the version of
@@ -42,7 +57,35 @@ func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 // If the key doesn't exist, Put installs the value if the
 // args.Version is 0, and returns ErrNoKey otherwise.
 func (kv *KVServer) Put(args *rpc.PutArgs, reply *rpc.PutReply) {
-	// Your code here.
+	key := args.Key
+	value := args.Value
+	version := args.Version
+	
+	kv.Mu.Lock()
+	defer kv.Mu.Unlock()
+	entry, ok := kv.Data[key]
+	if ok {
+		if version == entry.Version {
+			kv.Data[key] = KVEntry{
+				Value: value,
+				Version: entry.Version + 1,
+			}
+			reply.Err = rpc.OK
+		} else {
+			reply.Err = rpc.ErrVersion
+		}
+	} else {
+		if version == 0 {
+			kv.Data[key] = KVEntry{
+				Value: value,
+				Version: 1,
+			}
+			reply.Err = rpc.OK
+		} else {
+			reply.Err = rpc.ErrNoKey
+		}
+
+	}
 }
 
 // You can ignore Kill() for this lab
